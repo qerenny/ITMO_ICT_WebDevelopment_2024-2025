51cab31bc2a90cad5045f8bcca7535c79705e40b
Korchagin Vadim Sergeevich <89642064+qerenny@users.noreply.github.com>
2024-12-27 17:45:39 +0300

    Add CORS middleware to settings

--- students/k3341/laboratory_works/Korchagin_Vadim/laboratory_work_3/hotel/mysite/settings.py
+++ students/k3341/laboratory_works/Korchagin_Vadim/laboratory_work_3/hotel/mysite/settings.py
@@ -65,6 +65,7 @@ DJOSER = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -72,11 +73,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-MIDDLEWARE_CLASSES = (
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.BrokenLinkEmailsMiddleware',
-    'django.middleware.common.CommonMiddleware',
-)
 
 
 
